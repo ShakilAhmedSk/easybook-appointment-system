411c92bc53f6d2ff1c66b03d496287c6f25327ca
Shakil Ahmed Sk <shakilahmedsk35@gmail.com>
2025-05-01 13:46:07 +0530

    v0.1

--- Frontend/EasyBook/src/App.jsx
+++ Frontend/EasyBook/src/App.jsx
@@ -1,18 +1,13 @@
-import Navbar from "./assets/Component/Navbar"
-
-
+import Navbar from "./assets/Component/Navbar";
+import { Outlet } from 'react-router';
 
 function App() {
-  
-
   return (
     <>
-      <Navbar>
-
-      </Navbar>
-      
+      <Navbar />
+      <Outlet /> {/* Child routes like Home, About, etc. will render here */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Frontend/EasyBook/src/main.jsx
+++ Frontend/EasyBook/src/main.jsx
@@ -1,16 +1,26 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import './index.css';
+
+import App from './App';
+import Home from './assets/Pages/Home';
+import About from './assets/Pages/About';
+import Contact from './assets/Pages/Contact';
+import Services from './assets/Pages/Services';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />, // Layout with Navbar
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'about', element: <About /> },
+      { path: 'contact', element: <Contact /> },
+      { path: 'services', element: <Services /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
-  <>
-
-  <BrowserRouter>
-
-  <App />
-
-  </BrowserRouter>
-    
-  </>,
-)
+  <RouterProvider router={router} />
+);
